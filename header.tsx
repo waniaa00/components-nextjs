a07d545df060e82a729dfaf2ea551edb3177999e
wania akram <160944972+waniaa00@users.noreply.github.com>
2024-09-18 23:09:40 +0500

    Add files via upload

---
+++ header.tsx
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <h1>
+            This is header
+        </h1>
+    )
+};
+export default Header;
---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Header from "./components/header";
+import About from "./components/About";
+
+export default function Home(){
+  return(
+    <>
+    <h1>MY PRACTICE APP</h1>
+    <Header/>
+    <About/>
+    </>
+  )
+}
